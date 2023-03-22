ebf6c42d3774966f5a20acdb7ea10b7431221c6c
Anselme Dor <anselmedor@hotmail.be>
2023-03-22 14:01:27 +0100

    Displaying rows with img from db

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-export default function Row({ title, fetchUrl, isLargeRow = false }) {
+export default function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
   const base_url = "https://image.tmdb.org/t/p/original";
@@ -16,20 +16,19 @@ export default function Row({ title, fetchUrl, isLargeRow = false }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
   return (
-    <div>
-      <h3>{title}</h3>
-      {movies.map((movie) => (
-        <img
-          className={` ${isLargeRow && ""}`}
-          src={`${base_url}${
-            isLargeRow ? movie.poster_path : movie.backdrop_path
-          }`}
-          alt={movie.name}
-          key={movie.id}
-        />
-      ))}
+    <div className="ml-5">
+      <h3 className="font-[500] text-2xl">{title}</h3>
+      <div className="row_posters flex overflow-y-hidden overflow-x-scroll p-5">
+        {movies.map((movie) => (
+          <img
+            className="max-h-48 object-contain mr-4 w-full transition duration-500 hover:scale-110"
+            src={`${base_url}${movie.backdrop_path}`}
+            alt={movie.name}
+            key={movie.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
