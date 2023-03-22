c9c49da1c7a539e39e5823014d48edad49ffb95e
Anselme Dor <anselmedor@hotmail.be>
2023-03-22 11:29:02 +0100

    Remove console log, refactoring fetch function and small ui change

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ export default function Banner() {
   const [movie, setMovie] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
+    const fetchData = async () => {
       const res = await axios.get(requests.fetchNetflixOriginals);
       setMovie(
         res.data.results[
@@ -16,24 +16,22 @@ export default function Banner() {
         ]
       );
       return res;
-    }
+    };
     fetchData();
   }, []);
-
-  console.log(movie);
   return (
     <div
       style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backgroundSize: "cover",
         backgroundPosition: "center center",
       }}
       className="h-screen"
     >
       <div className="flex flex-col absolute bottom-60 left-10 gap-5">
-        <h3 className="text-4xl">
+        <h2 className="text-4xl">
           {movie?.title || movie?.name || movie?.original_name}
-        </h3>
+        </h2>
         <p className="w-3/4 xl:w-1/2"> {movie?.overview}</p>
         <div className="flex gap-8 text-xl font-[500]">
           <button className="flex items-center gap-3 bg-white text-black py-2 px-6 rounded-md">
