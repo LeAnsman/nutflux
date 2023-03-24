508f8c6e58668e0404644f9aa076570724f27f25
Anselme Dor <anselmedor@hotmail.be>
2023-03-24 14:52:12 +0100

    Reworking tv and movie page

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,45 +1,17 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Navbar } from "../components";
+import { Navbar, Row } from "../components";
 import requests from "../utils/Requests";
 
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
-
-  const [selectedFetch, setSelectedFetch] = useState(requests.fetchTopRated);
-
-  const base_url = "https://image.tmdb.org/t/p/original";
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(selectedFetch);
-      setMovies(res.data.results);
-      return res;
-    };
-
-    fetchData();
-  }, [selectedFetch]);
-
-  console.log(movies);
-
   return (
     <>
       <Navbar />
-      <main className="mt-36">
-        <select
-          className="mx-10"
-          onChange={(e) => setSelectedFetch(e.target.value)}
-        >
-          <option default value={requests.fetchTopRated}>
-            Top Rated
-          </option>
-          <option value={requests.fetchPopularMovies}>Popular</option>
-        </select>
-        <div>
-          {movies?.map((movie) => (
-            <div key={movie.id}>movie</div>
-          ))}
-        </div>
+      <main className="mt-36 mb-12">
+        <Row title={"Top Rated Movies"} fetchUrl={requests.fetchTopRated} />
+        <Row title={"Popular Movies"} fetchUrl={requests.fetchPopularTV} />
+        <Row title={"Action Movies"} fetchUrl={requests.fetchActionMovies} />
+        <Row title={"Comedy Movies"} fetchUrl={requests.fetchComedyMovies} />
+        <Row title={"Horror Movies"} fetchUrl={requests.fetchHorrorMovies} />
+        <Row title={"Romance Movies"} fetchUrl={requests.fetchRomanceMovies} />
       </main>
     </>
   );
--- src/pages/TV.jsx
+++ src/pages/TV.jsx
@@ -1,58 +1,13 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Navbar } from "../components";
+import { Navbar, Row } from "../components";
 import requests from "../utils/Requests";
 
 export default function TV() {
-  const [movies, setMovies] = useState([]);
-
-  const [selectedFetch, setSelectedFetch] = useState(requests.fetchTopRated);
-
-  const base_url = "https://image.tmdb.org/t/p/original";
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(selectedFetch);
-      setMovies(res.data.results);
-      return res;
-    };
-
-    fetchData();
-  }, [selectedFetch]);
-
-  console.log(movies);
-
   return (
     <>
       <Navbar />
       <main className="mt-36">
-        <select
-          className="mx-10"
-          onChange={(e) => setSelectedFetch(e.target.value)}
-        >
-          <option default value={requests.fetchTopRatedTV}>
-            Top Rated
-          </option>
-          <option value={requests.fetchPopularTV}>Popular</option>
-        </select>
-        <div className="mx-8 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8 2xl:grid-cols-5">
-          {movies?.map((movie) => (
-            <div key={movie.id} className="relative">
-              <img
-                src={
-                  movie.backdrop_path
-                    ? `${base_url}${movie.backdrop_path}`
-                    : `${base_url}${movie.poster_path}`
-                }
-                alt=""
-              />
-              <Link className="absolute bottom-2 left-2 font-[600] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-                {movie.name || movie.title}
-              </Link>
-            </div>
-          ))}
-        </div>
+        <Row title={"Top Rated TV Show"} fetchUrl={requests.fetchTopRatedTV} />
+        <Row title={"Popular TV Show"} fetchUrl={requests.fetchPopularTV} />
       </main>
     </>
   );
