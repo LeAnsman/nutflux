0e7495a9cd232947d3120867aa00079d98f093c8
Anselme Dor <anselmedor@hotmail.be>
2023-03-24 14:53:04 +0100

    Reworking home page (w/ tv and movie page)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,10 +20,6 @@ export default function Home() {
           title={"Netflix Originals"}
           fetchUrl={requests.fetchNetflixOriginals}
         />
-        <Row title={"Action Movies"} fetchUrl={requests.fetchActionMovies} />
-        <Row title={"Comedy Movies"} fetchUrl={requests.fetchComedyMovies} />
-        <Row title={"Horror Movies"} fetchUrl={requests.fetchHorrorMovies} />
-        <Row title={"Romance Movies"} fetchUrl={requests.fetchRomanceMovies} />
         <Row title={"Documentaries"} fetchUrl={requests.fetchDocumentaries} />
       </main>
     </>
